9c4df2ce47b3325c4ff61ececfe045d931dd72be
Rohit V <vrohit13@gmail.com>
2015-02-28 13:25:38 -0800

    Corrected files

---
+++ services/src/main/java/com/apollo/util/YahooEvent.java
@@ -0,0 +1,18 @@
+import java.util.Calendar;
+
+
+public class YahooEvent {
+	
+	int severity;
+	int count;
+	
+	String node;
+	String property;
+	Calendar firstOccurence;
+	
+	String summary;
+	
+	Calendar latestTime;
+	
+
+}
