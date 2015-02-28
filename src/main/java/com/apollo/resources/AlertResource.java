b9fa9352d666802bf988cffd77a77f6a33ab2c87
Mrugen Deshmukh <mrugen@Mrugens-MacBook-Pro.local>
2015-02-28 11:38:03 -0800

    moved resources to its own package

--- src/main/java/com/apollo/resources/AlertResource.java
+++ src/main/java/com/apollo/resources/AlertResource.java
@@ -1,4 +1,4 @@
-package com.apollo.services;
+package com.apollo.resources;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
